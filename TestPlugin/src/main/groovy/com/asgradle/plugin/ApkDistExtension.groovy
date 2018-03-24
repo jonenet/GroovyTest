a82c5af7c9b3291803d9cbd5d700e1243f9737b7
jonenet <jonenet0619@163.com>
2018-03-24 10:53:26 +0800

    Initial commit

---
+++ TestPlugin/src/main/groovy/com/asgradle/plugin/ApkDistExtension.groovy
@@ -0,0 +1,10 @@
+package com.asgradle.plugin
+/**
+ * Description: 
+ * Author     : jone
+ * Date       : 2018/3/17 23:21
+ */
+class ApkDistExtension {
+    Closure nameMap = null;
+    String destDir = null;
+}
---
+++ uploadLibrary/src/main/java/com/example/cjni/mylibrary/AddUtils.java
@@ -0,0 +1,15 @@
+package com.example.cjni.mylibrary;
+
+/**
+ * Description:
+ * Author     : jone
+ * Date       : 2018/3/14 12:47
+ */
+
+public class AddUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
